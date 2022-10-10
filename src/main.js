00673febbcd3bac61b996779f9a9457c25ffb656
BillPlayzToday <tonileoels@gmail.com>
2022-10-10 17:12:07 +0200

    Set background position to absolute + Create Camera, TextLabel, ImageLabel Classes

--- src/main.js
+++ src/main.js
@@ -2,12 +2,18 @@ export class LameEngine {
   constructor(viewport) {
     this.viewport = viewport
     this.background = null
+    this.camera = Camera()
   }
 
-  // VISUAL
+  // General
+  set_
+
+  // Visual
+  set_camera(camera) {
+    this.camera = camera
+  }
+
   set_background(visual) {
-    console.log(visual instanceof RGB)
-    console.log(visual instanceof Image)
     if (this.background) {
       this.background.remove()
       this.background = null
@@ -15,10 +21,10 @@ export class LameEngine {
 
     if (visual instanceof RGB) {
       this.background = document.createElement("div")
-      this.background.setAttribute("style","width: 100%; height: 100%; background-color: rgb(" + String(visual.r) + "," + String(visual.g) + "," + String(visual.b) + ");")
+      this.background.setAttribute("style","position: absolute; width: 100%; height: 100%; background-color: rgb(" + String(visual.r) + "," + String(visual.g) + "," + String(visual.b) + ");")
     } else if (visual instanceof Image) {
       this.background = document.createElement("img")
-      this.background.setAttribute("style","width: 100%; height: 100%;")
+      this.background.setAttribute("style","position: absolute; width: 100%; height: 100%;")
       this.background.setAttribute("src",visual.src)
     }
 
@@ -28,6 +34,7 @@ export class LameEngine {
   }
 }
 
+// Types
 export class RGB {
   constructor(r, g, b) {
     this.r = r
@@ -39,5 +46,56 @@ export class RGB {
 export class Image {
   constructor(src) {
     this.src = src
+  }
+}
+
+// Object Classes
+export class Camera {
+  constructor() {
+    this.x = 0
+    this.y = 0
+  }
+}
+
+export class Frame {
+  constructor() {
+    this.position = {
+      "x": 0,
+      "y": 0
+    }
+    this.size = {
+      "x": 0,
+      "y": 0
+    }
+    this.rotation = 0
+  }
+}
+
+export class ImageLabel {
+  constructor() {
+    this.position = {
+      "x": 0,
+      "y": 0
+    }
+    this.size = {
+      "x": 0,
+      "y": 0
+    }
+    this.rotation = 0
+  }
+}
+
+export class TextLabel {
+  constructor() {
+    this.position = {
+      "x": 0,
+      "y": 0
+    }
+    this.size = {
+      "x": 0,
+      "y": 0
+    }
+    this.rotation = 0
+    this.wrapped = 0
   }
 }
