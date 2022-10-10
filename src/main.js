e5a68ec7bf47fd8090ec8f2194aa2c1d912f5d7f
BillPlayzToday <tonileoels@gmail.com>
2022-10-10 16:04:13 +0200

    set_background, RGB class created, Image class created

--- src/main.js
+++ src/main.js
@@ -1,5 +1,47 @@
 export class LameEngine {
   constructor(viewport) {
     this.viewport = viewport
+    this.background = null
+  }
+
+  // VISUAL
+  set_background(visual) {
+    console.log(visual instanceof RGB)
+    console.log(visual instanceof Image)
+    if (this.background) {
+      this.background.remove()
+      this.background = null
+    }
+
+    if (visual instanceof RGB) {
+      this.background = document.createElement(
+        tagName = "div"
+      )
+      this.background.setAttribute("style","width: 100%; height: 100%; color: rgb(" + String(visual.r) + "," + String(visual.g) + "," + String(visual.b) + ");")
+    } else if (visual instanceof Image) {
+      this.background = document.createElement(
+        tagName = "img"
+      )
+      this.background.setAttribute("style","width: 100%; height: 100%;")
+      this.background.setAttribute("src",visual.src)
+    }
+
+    if (this.background) {
+      this.viewport.appendChild(this.background)
+    }
+  }
+}
+
+export class RGB {
+  constructor(r, g, b) {
+    this.r = r
+    this.g = g
+    this.b = b
+  }
+}
+
+export class Image {
+  constructor(src) {
+    this.src = src
   }
 }
